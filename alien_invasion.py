07e8acdccdba8dd9d558ace3e0566fd1c7c54171
sam-rsuarez10 <suarezzcode2001@gmail.com>
2021-11-07 00:02:12 -0300

    Display a row of aliens into the screen, increase screen's width and height

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,6 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf 
 
 def run_game():
@@ -18,16 +17,19 @@ def run_game():
     # Make a group to store bullets in
     bullets = Group()
 
-    # Make an alien
-    alien = Alien(ai_settings, screen)
+    # Make a group of aliens
+    aliens = Group()
+
+    # Create fleet of aliens
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # Start the main loop for the game
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets, alien)
-        gf.update_screen(ai_settings, screen, ship, bullets, alien)
+        gf.update_screen(ai_settings, screen, ship, bullets, aliens)
+        gf.update_screen(ai_settings, screen, ship, bullets, aliens)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,7 @@
 import sys 
 import pygame 
 from bullet import Bullet
+from alien import Alien
 
 
 def fire_bullet(ai_settings, screen, ship, bullets):
@@ -40,7 +41,7 @@ def check_events(ai_settings, screen, ship, bullets):
             elif event.type == pygame.KEYUP:
                 check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, bullets, alien):
+def update_screen(ai_settings, screen, ship, bullets, aliens):
     """Update images on the screen and flip to the new screen"""
 
     # Redraw the screen each gpass through the loop
@@ -49,7 +50,7 @@ def update_screen(ai_settings, screen, ship, bullets, alien):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
 
     # Make the most recently drawn screen visible
     pygame.display.flip()
@@ -65,3 +66,20 @@ def update_bullets(bullets):
             bullets.remove(bullet)
     #end for
 
+
+def create_fleet(ai_settings, screen, aliens):
+    ''' Create a full fleet of aliens '''
+    # Create an alien and find the number of aliens in a row 
+    # Spacing between each alien is equal to one alien width
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - (2 * alien_width)
+    number_aliens_x = int(ai_settings.screen_width / (2 * alien_width))
+
+    # Create first row of aliens
+    for alien_number in range(number_aliens_x):
+        # Create an alien and place it in the row
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.add(aliens)
