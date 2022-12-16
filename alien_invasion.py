8e228a5aaa8db75119c3fae0cae1226074b2eeac
sam-rsuarez10 <suarezzcode2001@gmail.com>
2022-12-16 19:20:30 -0300

    Freeze game when there are not ships left

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,10 +30,12 @@ def run_game():
     # Start the main loop for the game
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets, aliens, ai_settings, screen, ship)
-        gf.update_aliens(ai_settings, aliens, ship, stats, screen, bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets, aliens)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(bullets, aliens, ai_settings, screen, ship)
+            gf.update_aliens(ai_settings, aliens, ship, stats, screen, bullets)
+
         gf.update_screen(ai_settings, screen, ship, bullets, aliens)
 
 
