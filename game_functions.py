cab67308970377af881fb4b3833ab8df39709071
sam-rsuarez10 <suarezzcode2001@gmail.com>
2022-12-16 19:13:34 -0300

    Reset game when an alien hit the bottom of the screen

--- game_functions.py
+++ game_functions.py
@@ -147,6 +147,9 @@ def update_aliens(ai_settings, aliens, ship, stats, screen, bullets):
     # Look for alien-ship collisions
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+    
+    # Look for aliens hitting the bottom of the screen
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 # end def
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
@@ -164,3 +167,12 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
 
     # Pause
     sleep(0.5)
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """Check if any aliens have reached the bottom of the screen"""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # Treat this the same as if the ship got hit
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
