1d8daa3b65a7bc1c3358c5b3177e461906e7e691
sam-rsuarez10 <suarezzcode2001@gmail.com>
2022-12-20 13:49:58 -0300

    Fix play button funcionality and make mouse cursor invisible when playing

--- game_functions.py
+++ game_functions.py
@@ -32,7 +32,9 @@ def check_keyup_events(event, ship):
 
 def check_play_button(ai_settings, screen, stats, play_button, mouse_x, mouse_y, aliens, bullets, ship):
     ''' Starts a new game when the player clicks Play '''
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
+        pygame.mouse.set_visible(False)
         # Reset game statistics
         stats.reset_stats()
         stats.game_active = True
@@ -191,6 +193,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
     """Check if any aliens have reached the bottom of the screen"""
