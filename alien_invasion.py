d8df84944c4fadbe4ae59f8212828e27bb073181
sam-rsuarez10 <suarezzcode2001@gmail.com>
2022-12-19 22:47:30 -0300

    Add start game functionality when pressing play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,7 +34,7 @@ def run_game():
 
     # Start the main loop for the game
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, ship, bullets,  stats, play_button)
 
         if stats.game_active:
             ship.update()
